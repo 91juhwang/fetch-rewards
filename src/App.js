b44a6a7c30edae7564e9b357c6c1beaddd88fb99
James <91juhwang@gmail.com>
2020-12-12 14:17:56 -0500

    Initialize Project with MUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+// components
+import ListTable from './ListTable';
+import FilterChips from './FilterChips';
+// constants
+const DATA = require('./data/data.json')
 
-function App() {
+const App = () => {
+  const [selectedListIDs, setSelectedListIDs] = useState([])
+  // sorting by the listID first then by the name
+  const sortedList = DATA.filter(data => !!data.name).sort((a, b) => {
+    if (a.listId > b.listId) {
+      return 1
+    }
+    if (b.listId > a.listId) {
+      return -1
+    }
+    // using .id instead of .name as the `name` contains `ID` as the suffix numeric value
+    if (a.id > b.id) {
+      return 1
+    }
+    if (b.id > a.id) {
+      return -1
+    }
+    return 0
+  });
+  const list = sortedList.filter(data => selectedListIDs.includes(data.listId))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FilterChips setSelectedListIDs={setSelectedListIDs} selectedListIDs={selectedListIDs} />
+      <ListTable list={list} />
+      {/* pagination */}
     </div>
   );
-}
+};
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 export default App;
